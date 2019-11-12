8f055a53f8954a2f9385f6614d8f477d8421dfd9
Juan Sebastian Salazar Giraldo <juan.salazar@natgeo.com>
2019-11-11 19:32:44 -0500

    Added debug mode

--- django_test/settings.py
+++ django_test/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lh)j&t&japl)h0by%#@$fubo=1_4p%4#an#9s-z89p#37iy57f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_MODE
 
 ALLOWED_HOSTS = [ALLOWED_HOST]
 
